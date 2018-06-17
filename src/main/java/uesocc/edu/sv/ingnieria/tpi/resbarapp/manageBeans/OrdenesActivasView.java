9b79e658ec51cfda3de482b5ed7abc0d4988a52a
Irvin Ayala <hypleyone@gmail.com>
2018-06-16 18:29:44 -0600

    Avanzando en ordenes activas

--- src/main/java/uesocc/edu/sv/ingnieria/tpi/resbarapp/manageBeans/OrdenesActivasView.java
+++ src/main/java/uesocc/edu/sv/ingnieria/tpi/resbarapp/manageBeans/OrdenesActivasView.java
@@ -6,6 +6,7 @@
 package uesocc.edu.sv.ingnieria.tpi.resbarapp.manageBeans;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.util.Map;
 import org.primefaces.model.SortOrder;
 import java.util.ArrayList;
@@ -29,39 +30,18 @@ import sv.edu.uesocc.disenio2018.resbar.backend.entities.Orden;
 @ManagedBean
 @Named(value = "ordenesActivasView")
 @ViewScoped
-public class OrdenesActivasView implements Serializable{
+public class OrdenesActivasView implements Serializable {
 
     private LazyDataModel<Orden> modelo;
     private Orden registro;
-    private double total;
+    private double valorSpinner;
+    private double cambio;
+    private List<Orden> listaOrdenes;
 
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
     /**
      * Creates a new instance of OrdenesActivasview
      */
     public OrdenesActivasView() {
-    }
-
-    public LazyDataModel<Orden> getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(LazyDataModel<Orden> modelo) {
-        this.modelo = modelo;
-    }
-
-    public Orden getRegistro() {
-        return registro;
-    }
-
-    public void setRegistro(Orden registro) {
-        this.registro = registro;
     }
 
     @PostConstruct
@@ -97,8 +77,8 @@ public class OrdenesActivasView implements Serializable{
                 public List<Orden> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
                     List<Orden> salida = new ArrayList();
                     try {
-                        this.setRowCount(ManejadorOrdenes.ObtenerActivas().size());        
-                        salida= ManejadorOrdenes.ObtenerActivas();
+                        this.setRowCount(ManejadorOrdenes.ObtenerActivas().size());
+                        salida = ManejadorOrdenes.ObtenerActivas();
                     } catch (Exception e) {
                         Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
                     }
@@ -111,5 +91,54 @@ public class OrdenesActivasView implements Serializable{
         }
     }
 
+    public void calcularCambio() {
+        this.setCambio(this.valorSpinner - Double.parseDouble(this.getRegistro().getTotal().toString()));
+    }
+
+    public void buscarOrdenes(String dato) {
+        this.setListaOrdenes(ManejadorOrdenes.BuscarActivas(dato));
+    }
+
+    //Getter y Setter
+    public double getCambio() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        return Double.parseDouble(df.format(cambio));
+    }
+
+    public void setCambio(double cambio) {
+        this.cambio = cambio;
+    }
+
+    public double getValorSpinner() {
+        return valorSpinner;
+    }
+
+    public void setValorSpinner(double valorSpinner) {
+        this.valorSpinner = valorSpinner;
+    }
+
+    public List<Orden> getListaOrdenes() {
+        return listaOrdenes;
+    }
+
+    public void setListaOrdenes(List<Orden> listaOrdenes) {
+        this.listaOrdenes = listaOrdenes;
+    }
+
+    public LazyDataModel<Orden> getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(LazyDataModel<Orden> modelo) {
+        this.modelo = modelo;
+    }
+
+    public Orden getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(Orden registro) {
+        this.registro = registro;
+    }
 
 }
