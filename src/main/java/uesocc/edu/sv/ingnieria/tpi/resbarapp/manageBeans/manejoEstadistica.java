f69c1fe6fb14c8414ea1bf643df202767366d769
Ngochez95 <nhgochez95@gmail.com>
2018-06-18 12:56:03 -0600

    Estadisticas por fecha

--- src/main/java/uesocc/edu/sv/ingnieria/tpi/resbarapp/manageBeans/manejoEstadistica.java
+++ src/main/java/uesocc/edu/sv/ingnieria/tpi/resbarapp/manageBeans/manejoEstadistica.java
@@ -10,16 +10,12 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
-import org.primefaces.PrimeFaces;
 import org.primefaces.event.SelectEvent;
-import sv.edu.uesocc.disenio2018.resbar.backend.controller.ManejadorCategorias;
 import sv.edu.uesocc.disenio2018.resbar.backend.controller.ManejadorOrdenes;
-import sv.edu.uesocc.disenio2018.resbar.backend.controller.ManejadorProductos;
 import sv.edu.uesocc.disenio2018.resbar.backend.entities.Orden;
 
 /**
@@ -29,61 +25,46 @@ import sv.edu.uesocc.disenio2018.resbar.backend.entities.Orden;
 @Named(value = "manejoEstadistica")
 @ViewScoped
 public class manejoEstadistica implements Serializable {
-
-    private List<Orden> VentasDiarias;
-    private Orden RegistroOrdenes;
-    private Date dia;
-//       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//     private Date dia;     
-//
-//    public manejoEstadistica() throws ParseException {
-//        this.dia = sdf.parse("2018-05-20");
-//    }
-    public manejoEstadistica(){
-    }
-    //@PostConstruct
-    public void ventasD() {
-        
-        this.VentasDiarias = ManejadorOrdenes.ObtenerVentas(dia);
-    }
+   ManejadorOrdenes manejadorOrden= new ManejadorOrdenes();
+    private List<Orden> historico;
+    private Date date1;
+    private Date fecha1;
+    private Date fecha2;
+    String fechaSeleccionada;
     
-    public void ObtenerObjeto(SelectEvent se) {
+    public void onDateSelect(SelectEvent event) throws ParseException {
 
-        this.RegistroOrdenes = (Orden)(se.getObject());
-    }
-
-    public List<Orden> getVentasDiarias() {
-        return VentasDiarias;
-    }
-
-    public void onDateSelect(SelectEvent event) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
-        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
-                this.VentasDiarias = ManejadorOrdenes.ObtenerVentas(dia);
-
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", dateFormat.format(event.getObject())));
+        fechaSeleccionada = dateFormat.format(event.getObject());
+        fecha1 = dateFormat.parse(fechaSeleccionada);
+        System.out.println("FECHA1=" + fecha1);
+        fecha2 = dateFormat.parse(fechaSeleccionada);
+        fecha2.setHours(23);
+        fecha2.setMinutes(59);
+        fecha2.setSeconds(59);
+        System.out.println("fecha2=" + fecha2);
+        historico = ManejadorOrdenes.ObtenerVentas(fecha1,fecha2);
+        System.out.println(historico);
     }
 
-
-    public void setVentasDiarias(List<Orden> VentasDiarias) {
-        this.VentasDiarias = VentasDiarias;
+    public Date getDate1() {
+        return date1;
     }
 
-    public Orden getRegistroOrdenes() {
-        return RegistroOrdenes;
+    public void setDate1(Date date1) {
+        this.date1 = date1;
     }
 
-    public void setRegistroOrdenes(Orden RegistroOrdenes) {
-        this.RegistroOrdenes = RegistroOrdenes;
+    public List<Orden> getHistorico() {
+        return historico;
     }
 
-    public Date getDia() {
-        return dia;
+    public void setHistorico(List<Orden> historico) {
+        this.historico = historico;
     }
-
-    public void setDia(Date dia) {
-        this.dia = dia;
-    }
-
 
 }
+
+    
